d2fe7d6175355d959c705f882d844135224ae2c5
Andrey Bychkov <mrdrew@altlinux.org>
2018-07-27 16:12:43 +0300

    Re-write base client and server config

--- ipalib/basentpconf.py
+++ ipalib/basentpconf.py
@@ -21,26 +21,30 @@ from ipalib.ntpmethods import TIME_SERVICE
 logger = getLogger(__name__)
 
 
-class BaseNTPClient(object):
-    def __init__(self, fstore=None, ntp_confile=None, ntp_bin=None,
-                 statestore=None, cli_domain=None, timeout=None, flag=None,
-                 ntp_servers=None, ntp_pool=None, args=None):
-
-        self.fstore = fstore
-        self.ntp_confile = ntp_confile
-        self.ntp_bin = ntp_bin
-        self.statestore = statestore
-        self.cli_domain = cli_domain
-
-        self.ntp_servers = ntpmethods.search_ntp_servers(
-            self.statestore,
-            self.cli_domain
+class BaseNTPClass(service.Service):
+    def __init__(self, service_name, fstore=None, sstore=None,
+                 ntp_confile=None, ntp_servers=None, ntp_pool=None,
+                 local_srv=None, fudge=None, cli_domain=None, timeout=None,
+                 flag=None, args=None, ntp_bin=None):
+        super(BaseNTPClass, self).__init__(
+            service_name=service_name,
+            fstore=fstore,
+            sstore=sstore,
+            service_desc="NTP daemon",
         )
 
+        self.ntp_confile = ntp_confile
+        self.ntp_servers = ntp_servers
+        self.ntp_bin = ntp_bin
+
         if not self.ntp_servers:
-            self.ntp_servers = ntp_servers
+            self.ntp_servers = ntpmethods.search_ntp_servers(self.sstore,
+                                                             self.cli_domain)
 
         self.ntp_pool = ntp_pool
+        self.local_srv = local_srv
+        self.fudge = fudge
+        self.cli_domain = cli_domain
         self.timeout = timeout
         self.flag = flag
         self.args = args
@@ -49,115 +53,86 @@ class BaseNTPClient(object):
             self.args = [paths.BIN_TIMEOUT, self.timeout,
                          self.ntp_bin, self.flag, self.ntp_confile]
 
+        if not self.fstore:
+            sysrestore.FileStore(paths.SYSRESTORE)
+
     def __configure_ntp(self):
         logger.debug("Configuring %s", TIME_SERVICE)
-        if not self.ntp_servers:
+
+        if self.ntp_servers or self.ntp_pool:
+            config_content = ntpmethods.set_config(self.ntp_confile, self.ntp_pool, self.ntp_servers)
+        elif self.local_srv:
+            config_content = ntpmethods.set_config(self.ntp_confile, servers=self.local_srv, fudge=self.fudge)
+        else:
+            config_content = None
             logger.warning("No SRV records of NTP servers found and "
                            "no NTP server or pool address was provided.")
+            logger.info("Using default %s configuration", TIME_SERVICE)
 
-        config_content = ntpmethods.set_config(self.ntp_confile,
-                                               servers=self.ntp_servers)
+        logger.debug("Backing up state %s", TIME_SERVICE)
+
+        enabled = ntpmethods.is_enabled()
+        running = ntpmethods.is_running()
+
+        self.sstore.backup_state(ntpmethods.ntp_service['service'], 'enabled', enabled)
+        self.sstore.backup_state(ntpmethods.ntp_service['service'], 'running', running)
+
+        ntpmethods.ntp_service['api'].stop()
+
+        if not config_content:
+            return False
 
         logger.debug("Backing up %s", self.ntp_confile)
         ntpmethods.backup_config(self.ntp_confile, self.fstore)
 
-        logger.debug("Backing up state %s", TIME_SERVICE)
-
-        enabled = ntpmethods.ntp_service['api'].is_enabled()
-        running = ntpmethods.ntp_service['api'].is_running()
-
-        self.statestore.backup_state(ntpmethods.ntp_service['service'],
-                                     'enabled', enabled)
-        self.statestore.backup_state(ntpmethods.ntp_service['service'],
-                                     'running', running)
-
         logger.debug("Writing configuration to %s", self.ntp_confile)
-        ntpmethods.ntp_service['api'].stop()
         ntpmethods.write_config(self.ntp_confile, config_content)
 
         tasks.restore_context(self.ntp_confile)
 
+        return True
+
     def sync_time(self):
-        configured = False
+
+        if not self.__configure_ntp():
+            ntpmethods.service_control.print_msg("Using default %s configuration", TIME_SERVICE)
+        else:
+            ntpmethods.service_control.print_msg("Successfully configuring %s", TIME_SERVICE)
+
+        if self.ntp_bin:
+            if not os.path.exists(self.ntp_bin):
+                return False
+
+        if self.args:
+            try:
+                logger.info("Attempting to sync time with %s", TIME_SERVICE)
+                logger.info("Will timeout after %s seconds", TIME_SERVICE)
+
+                ipautil.run(self.args)
+
+                ntpmethods.ntp_service['api'].enable()
+                ntpmethods.ntp_service['api'].start()
+
+                return True
+
+            except ipautil.CalledProcessError as e:
+                if e.returncode == 124:
+                    logger.debug("Process did not complete before timeout")
+
+                return False
 
         try:
-            self.__configure_ntp()
-            configured = True
-        except Exception:
-            pass
+            self.step("stopping %s" % self.service_name, self.stop)
+            self.step("writing configuration", self.__configure_ntp)
+            self.step("configuring %s to start on boot"
+                      % self.service_name, self.enable)
+            self.start("starting %d" % self.service_name, self.start)
 
-        if not configured:
-            logger.info("Using default %s configuration", TIME_SERVICE)
-
-        if not os.path.exists(self.ntp_bin):
-            return False
-
-        try:
-            logger.info("Attempting to sync time with %s", TIME_SERVICE)
-            logger.info("Will timeout after %s seconds", TIME_SERVICE)
-
-            ipautil.run(self.args)
-
-            ntpmethods.ntp_service['api'].enable()
-            ntpmethods.ntp_service['api'].start()
+            self.start_creation()
 
             return True
-
-        except ipautil.CalledProcessError as e:
-            if e.returncode == 124:
-                logger.debug("Process did not complete before timeout")
-
+        except Exception:
             return False
 
     def uninstall(self):
-        ntpmethods.uninstall(self.statestore, self.fstore,
-                             self.ntp_confile, logger)
-
-
-class BaseNTPServer(service.Service):
-    def __init__(self, service_name, ntp_confile=None, fstore=None,
-                 ntp_servers=None, ntp_pool=None, local_srv=None,
-                 fudge=None, sstore=None):
-        super(BaseNTPServer, self).__init__(
-            service_name=service_name,
-            fstore=fstore,
-            service_desc="NTP daemon",
-            sstore=sstore,
-        )
-
-        self.ntp_confile = ntp_confile
-        self.ntp_servers = ntp_servers
-        self.ntp_pool = ntp_pool
-        self.local_srv = local_srv
-        self.fudge = fudge
-
-        if not fstore:
-            self.fstore = sysrestore.FileStore(paths.SYSRESTORE)
-
-    def __configure_ntp(self):
-        logger.debug("Make backup")
-        self.fstore.backup_file(self.ntp_confile)
-
-        logger.debug("Configuring")
-        content = ntpmethods.set_config(self.ntp_confile,
-                                        servers=self.local_srv,
-                                        fudge=self.fudge)
-
-        logger.debug("Write config")
-
-        ntpmethods.ntp_service['api'].stop()
-        ntpmethods.write_config(self.ntp_confile, content)
-
-    def sync_time(self):
-        self.step("stopping %s" % self.service_name, self.stop)
-        self.step("writing configuration", self.__configure_ntp)
-        self.step("configuring %s to start on boot"
-                  % self.service_name, self.enable)
-        self.start("starting %d" % self.service_name, self.start)
-
-        self.start_creation()
-
-    def uninstall(self):
-        ntpmethods.uninstall(
-            self.sstore, self.fstore, self.ntp_confile, logger
-        )
+        ntpmethods.uninstall(self.sstore, self.fstore, self.ntp_confile, logger)
